fe26eb36ee5d0909173675a8924edabf8e6abb4b
sachin durge <sachindurge@yahoo.com>
2017-09-05 10:17:07 +0530

    Added a sample application function.

--- src/mod/applications/mod_sample/mod_sample.c
+++ src/mod/applications/mod_sample/mod_sample.c
@@ -4,10 +4,37 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_sample_load);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_sample_shutdown);
 SWITCH_MODULE_RUNTIME_FUNCTION(mod_sample_runtime);
 
-SWITCH_MODULE_DEFINITION(mod_sample, mod_sample_load, mod_sample_shutdown, mod_sample_runtime);
+static struct{
+	int debug;
+	int32_t threads;
+	int32_t running;
+	switch_mutex_t *mutex;
+	switch_memory_pool_t *pool;
+} globals;
+
+SWITCH_MODULE_DEFINITION(mod_sample, mod_sample_load, mod_sample_shutdown, NULL);
+
+SWITCH_STANDARD_APP(sample_app)
+{
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ALERT, "Sample application has been loaded.");
+	return;
+}
 
 SWITCH_MODULE_LOAD_FUNCTION(mod_sample_load)
 {
+	switch_application_interface_t *app_interface;
+	
+	memset(&globals, 0, sizeof(globals));
+	globals.pool = pool;
+	
+	globals.running = 1;
+	
+	*module_interface = switch_loadable_module_create_module_interface(pool, modname);
+	
+	SWITCH_ADD_APP(app_interface, "sample", "Sample module loaded.", NULL, sample_app, NULL, SAF_NONE);
+	
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Sample module loaded successfully!!");
+	
 	return SWITCH_STATUS_SUCCESS;
 }
 
