9dfa97b752b413a88494de1445bd223934ce0426
sachin durge <sachindurge@yahoo.com>
2017-09-05 15:17:42 +0530

    Added a API function.

--- src/mod/applications/mod_sample/mod_sample.c
+++ src/mod/applications/mod_sample/mod_sample.c
@@ -20,18 +20,24 @@ SWITCH_STANDARD_APP(sample_app)
 	return;
 }
 
+SWITCH_STANDARD_API(sample_api)
+{
+	return SWITCH_STATUS_SUCCESS;
+}
 SWITCH_MODULE_LOAD_FUNCTION(mod_sample_load)
 {
 	switch_application_interface_t *app_interface;
+	switch_api_interface_t *api_interface;
 	
 	memset(&globals, 0, sizeof(globals));
 	globals.pool = pool;
-	
+
 	globals.running = 1;
-	
+
 	*module_interface = switch_loadable_module_create_module_interface(pool, modname);
-	
+
 	SWITCH_ADD_APP(app_interface, "sample", "Sample module loaded.", NULL, sample_app, NULL, SAF_NONE);
+	SWITCH_ADD_API(api_interface, "sample", "Sample of module", sample_api, SAF_NONE);
 	
 	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Sample module loaded successfully!!");
 	
@@ -45,5 +51,5 @@ SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_sample_shutdown)
 
 SWITCH_MODULE_RUNTIME_FUNCTION(mod_sample_runtime)
 {
-	return SWITCH_STATUS_SUCCESS;	
+	return SWITCH_STATUS_SUCCESS;
 }
